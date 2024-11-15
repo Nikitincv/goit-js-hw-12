f0b4290d2cfcac5a34c734052a18810cf4131bd9
Nikitincv <161200278+Nikitincv@users.noreply.github.com>
2024-11-15 11:50:29 +0000

    Deploying to gh-pages from @ Nikitincv/goit-js-hw-12@31eea70c36c6b7e5d913188f8743a37ba6e226a3 🚀

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-/* empty css                      */(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+/* empty css                      */import{a as L,S as w,i as o}from"./assets/vendor-DgUTYsMS.js";(function(){const a=document.createElement("link").relList;if(a&&a.supports&&a.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))n(t);new MutationObserver(t=>{for(const s of t)if(s.type==="childList")for(const u of s.addedNodes)u.tagName==="LINK"&&u.rel==="modulepreload"&&n(u)}).observe(document,{childList:!0,subtree:!0});function e(t){const s={};return t.integrity&&(s.integrity=t.integrity),t.referrerPolicy&&(s.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?s.credentials="include":t.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function n(t){if(t.ep)return;t.ep=!0;const s=e(t);fetch(t.href,s)}})();async function f(r,a){const e=new URLSearchParams({key:"45431575-a54d9a4f673da9f6ecaf0fabb",page:a,per_page:15,q:r,image_type:"photo",orientation:"horizontal",safesearch:!0}),{data:n}=await L(`https://pixabay.com/api/?${e}`);return n}const b=new w(".gallery a");function h(r){const a=r.map(e=>`
+    <li class='gallery-item'><a class="gallery-link"  href="${e.largeImageURL}">
+    <img class="gallery-img" src="${e.webformatURL}" alt="${e.tags}">
+      </a>
+    <ul class="card-info">
+    <li class="card-info-item">
+      <p>Like:${e.likes}</p>
+    </li>
+    <li class="card-info-item">
+      <p>Views:${e.views}</p>
+    </li>
+    <li class="card-info-item">
+      <p>Coments:${e.comments}</p>
+    </li>
+    <li class="card-info-item">
+      <p>Downloads:${e.downloads}</p>
+    </li></ul>
+  </li>`).join("");m.insertAdjacentHTML("beforeend",a),b.refresh()}const l=document.querySelector(".loader"),d=document.querySelector(".load-more"),p=document.querySelector(".search-form"),m=document.querySelector(".gallery");let g=0,i=1,c="";p.addEventListener("submit",S);async function S(r){r.preventDefault(),m.innerHTML="";const{search:a}=r.currentTarget.elements;if(c=a.value.trim(),i=1,c==="")return o.error({position:"topRight",message:"Sorry, there are no images matching your search query. Please try again!"});l.classList.remove("isheaden");try{const e=await f(c,i);if(e.hits.length===0)return d.classList.add("isheaden"),o.warning({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"});h(e.hits),y(),g=Math.ceil(e.totalHits/15),p.reset(),e.totalHits>15&&d.classList.remove("isheaden")}catch(e){o.error({position:"topRight",message:e.message})}finally{l.classList.add("isheaden")}}d.addEventListener("click",v);async function v(){l.classList.remove("isheaden"),i+=1;try{const r=await f(c,i);if(h(r.hits),y(),g===i)return d.classList.add("isheaden"),o.warning({message:"We're sorry, but you've reached the end of search results.",position:"topRight"})}catch(r){o.error({position:"topRight",message:r.message})}finally{l.classList.add("isheaden")}}function y(){const{height:r}=m.firstElementChild.getBoundingClientRect();console.log(r),window.scrollBy({top:r*2,behavior:"smooth"})}
 //# sourceMappingURL=index.js.map
